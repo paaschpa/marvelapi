cc4e5c32cc41b9b0e82b4dcc92ff67b74a4c79bd
paaschpa <paaschpa@gmail.com>
2014-09-08 11:14:53 -0500

    add simple (and incomplete) room directions

---
+++ MarvelApi/Models/Room.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MarvelApi.Models
+{
+    public class Room
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public IDictionary<string, Guid> Neighbors { get; set; }
+    }
+}
---
+++ MarvelApi/Services/RoomService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MarvelApi.Models;
+using ServiceStack;
+
+namespace MarvelApi.Services
+{
+    [Route("/Room")]
+    public class RoomRequest
+    {
+        public Guid FindId { get; set; }
+    }
+
+    public class RoomService : Service
+    {
+        public Room Get(RoomRequest request)
+        {
+            if (request.FindId == Guid.Empty)
+            {
+                return new Room()
+                {
+                    Id = Guid.NewGuid(),
+                    Description = "Go North young man!",
+                    Neighbors = new Dictionary<string, Guid>
+                        {
+                            {"N", Guid.Empty},
+                            {"S", Guid.Empty},
+                            {"E", Guid.Empty},
+                            {"W", Guid.Empty}
+                        }
+                };
+            }
+            return new Room()
+                {
+                    Id = Guid.NewGuid(),
+                    Description = "So you like reading comics do ya?",
+                    Neighbors = new Dictionary<string, Guid>
+                        {
+                            {"N", Guid.Empty},
+                            {"S", Guid.Empty},
+                            {"E", Guid.Empty},
+                            {"W", Guid.Empty}
+                        }
+                };
+        }
+       
+    }
+}
